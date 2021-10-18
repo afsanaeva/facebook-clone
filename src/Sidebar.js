67fc2494a571fe7c993121963f7612bd3c52e49d
afsanaeva <afsanaeva05@gmail.com>
2021-10-18 23:18:29 +0600

    sidebar done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarRow from './SidebarRow';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow title='Covid-19 Information Center'/>
-            <SidebarRow title='Pages'/>
-            <SidebarRow title='Friends'/>
-            <SidebarRow title='Messenger'/>
-            <SidebarRow title='Marketplace'/>
-            <SidebarRow title='Videos'/>
+            <SidebarRow src='https://avatars.githubusercontent.com/u/41413300?v=4' title='Afsana Eva'/> 
+            <SidebarRow Icon={LocalHospitalIcon} title='Covid-19 Information Center'/>
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages'/>
+            <SidebarRow Icon={PeopleIcon} title='Friends'/>
+            <SidebarRow Icon={ChatIcon} title='Messenger'/>
+            <SidebarRow Icon={StorefrontIcon} title='Marketplace'/>
+            <SidebarRow Icon={VideoLibraryIcon} title='Videos'/>
+            <SidebarRow Icon={ExpandMoreIcon} title='Marketplace'/>
         </div>
     )
 }
